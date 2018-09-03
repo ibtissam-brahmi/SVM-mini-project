83529c766777e6d3da6ad3e721a1826182a4f04d
BrahmiIbtissam <ibtissambrahmi@gmail.com>
2018-09-03 01:50:46 +0100

    create_classifier

--- svm_author_id.py
+++ svm_author_id.py
@@ -24,5 +24,6 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
-
+from sklearn import svm
+clf=svm.SVC()
 #########################################################
