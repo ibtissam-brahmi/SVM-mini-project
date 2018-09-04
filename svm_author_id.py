08bdbe4ecded530030927586731d814e62cffd8b
BrahmiIbtissam <ibtissambrahmi@gmail.com>
2018-09-04 01:01:11 +0100

    some_simple_test_questions

--- svm_author_id.py
+++ svm_author_id.py
@@ -24,6 +24,9 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+"""
+    Create the optimized classifier (Highest accuracy) and fit it with our training sets
+"""
 from sklearn import svm
 from sklearn.metrics import accuracy_score
 #labels_train = labels_train[:len(labels_train)/100]
@@ -37,5 +40,21 @@ print ("predicting time:", round(time()-t0, 3), "s")
 accuracy = accuracy_score(pred,labels_test)
 print ("the accuracy of this classifier is", accuracy)
 
+"""
+    Extract predicitions for element 10 of the test set, the 26th and the 50th
+"""
+pred1 = labels_test[10]
+pred2 = labels_test[26]
+pred3 = labels_test[50]
+print("the prediction of elements 10, 26 and 50 of the test set are, respectively", pred1, pred2, pred3)
 
+"""
+There are over 1700 test events how many are predicted to be in the 'Chris' (1) class?
+"""
+chris_occ=0
+for i in range(len(pred)):
+    if pred[i]==1:
+        chris_occ+=1
+
+print ("There are {} predicted to be in the Chris class".format(chris_occ))
 #########################################################
